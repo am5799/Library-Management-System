c21297ead05398b908fcf84ab14af77da0b98072
raydbasa <s441006995@st.uqu.edu.sa>
2023-12-30 00:39:33 +0300

    add-Deposit

--- librarysystem/urls.py
+++ librarysystem/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('books/', include('bookmanagement.urls')),
     path('users/', include('usermanagement.urls')),
